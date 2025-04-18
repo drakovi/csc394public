65d371cc43f137946d4de99b75084913eb31b786
root <root@DESKTOP-102OEE0.>
2025-04-18 11:01:54 -0500

    Uploaded temp DB

---
+++ cyberlawdb.sql
@@ -0,0 +1,81 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
+--
+-- Host: localhost    Database: cyberlawdb
+-- ------------------------------------------------------
+-- Server version	8.0.41-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `us_cybersecurity_laws`
+--
+
+DROP TABLE IF EXISTS `us_cybersecurity_laws`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `us_cybersecurity_laws` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `category` enum('Compliance','Data Privacy','Cybercrime Consequences') DEFAULT NULL,
+  `description` text,
+  `citation` text,
+  `url` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `us_cybersecurity_laws`
+--
+
+LOCK TABLES `us_cybersecurity_laws` WRITE;
+/*!40000 ALTER TABLE `us_cybersecurity_laws` DISABLE KEYS */;
+INSERT INTO `us_cybersecurity_laws` VALUES (1,'Health Insurance Portability and Accountability Act (HIPAA)','Compliance','HIPAA mandates data privacy and security provisions for safeguarding medical information.','\"Health Insurance Portability and Accountability Act.\" U.S. Department of Health & Human Services, www.hhs.gov/hipaa/index.html.','https://www.hhs.gov/hipaa/index.html'),(2,'California Consumer Privacy Act (CCPA)','Data Privacy','CCPA grants California residents new rights regarding their personal information and imposes data protection responsibilities on businesses.','\"California Consumer Privacy Act.\" State of California - Department of Justice, oag.ca.gov/privacy/ccpa.','https://oag.ca.gov/privacy/ccpa'),(3,'Computer Fraud and Abuse Act (CFAA)','Cybercrime Consequences','CFAA criminalizes unauthorized access to computer systems and has been used in various cybercrime prosecutions.','\"Computer Fraud and Abuse Act.\" Legal Information Institute, Cornell Law School, www.law.cornell.edu/uscode/text/18/1030.','https://www.law.cornell.edu/uscode/text/18/1030');
+/*!40000 ALTER TABLE `us_cybersecurity_laws` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password_hash` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'pete','6cb912de5b6ef87cb3ecf92c13052d80d02ed91771e6e640625d759cd444e941'),(4,'ppp','c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7'),(5,'admin','08f2b4a83c6d31deef5357b7956fde1086c7fbbf5c8d11b879bbae880806d0fd');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-18 11:00:43
