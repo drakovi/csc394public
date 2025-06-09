4ce19a3d54644cb0c14a90d99c2a03784fa10d70
root <root@DESKTOP-102OEE0.localdomain>
2025-06-09 13:17:23 -0500

    Added an older version of DB to be compatible

---
+++ clean_dump.sql
@@ -0,0 +1,99 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `bookmarks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookmarks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `citation` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `bookmarks` WRITE;
+/*!40000 ALTER TABLE `bookmarks` DISABLE KEYS */;
+INSERT INTO `bookmarks` VALUES (5,'pete','Justice.gov. \'Computer Fraud and Abuse Act.\' U.S. Department of Justice. https://www.justice.gov/criminal-ccips/file/904941/download');
+/*!40000 ALTER TABLE `bookmarks` ENABLE KEYS */;
+UNLOCK TABLES;
+DROP TABLE IF EXISTS `search_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `search_history` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `query` text,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `search_history` WRITE;
+/*!40000 ALTER TABLE `search_history` DISABLE KEYS */;
+INSERT INTO `search_history` VALUES (1,'pete','hackers','2025-05-26 16:48:12');
+INSERT INTO `search_history` VALUES (2,'pete','Health privacy concerns with online data','2025-05-29 23:36:36');
+INSERT INTO `search_history` VALUES (3,'pete','Does a gym keep my information after quitting?','2025-05-30 00:01:30');
+INSERT INTO `search_history` VALUES (4,'pete','Health privacy concerns with online data','2025-05-30 00:06:36');
+INSERT INTO `search_history` VALUES (5,'pete','hackers','2025-05-30 00:11:43');
+INSERT INTO `search_history` VALUES (6,'pete','hackers','2025-06-09 18:09:34');
+/*!40000 ALTER TABLE `search_history` ENABLE KEYS */;
+UNLOCK TABLES;
+DROP TABLE IF EXISTS `us_cybersecurity_laws`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `us_cybersecurity_laws` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `category` enum('Compliance','Data Privacy','Cybercrime Consequences') DEFAULT NULL,
+  `description` text,
+  `citation` text,
+  `url` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `us_cybersecurity_laws` WRITE;
+/*!40000 ALTER TABLE `us_cybersecurity_laws` DISABLE KEYS */;
+INSERT INTO `us_cybersecurity_laws` VALUES (1,'Health Insurance Portability and Accountability Act (HIPAA)','Compliance','HIPAA mandates data privacy and security provisions for safeguarding medical information.','\"Health Insurance Portability and Accountability Act.\" U.S. Department of Health & Human Services, www.hhs.gov/hipaa/index.html.','https://www.hhs.gov/hipaa/index.html');
+INSERT INTO `us_cybersecurity_laws` VALUES (2,'California Consumer Privacy Act (CCPA)','Data Privacy','CCPA grants California residents new rights regarding their personal information and imposes data protection responsibilities on businesses.','\"California Consumer Privacy Act.\" State of California - Department of Justice, oag.ca.gov/privacy/ccpa.','https://oag.ca.gov/privacy/ccpa');
+INSERT INTO `us_cybersecurity_laws` VALUES (3,'Computer Fraud and Abuse Act (CFAA)','Cybercrime Consequences','CFAA criminalizes unauthorized access to computer systems and has been used in various cybercrime prosecutions.','\"Computer Fraud and Abuse Act.\" Legal Information Institute, Cornell Law School, www.law.cornell.edu/uscode/text/18/1030.','https://www.law.cornell.edu/uscode/text/18/1030');
+/*!40000 ALTER TABLE `us_cybersecurity_laws` ENABLE KEYS */;
+UNLOCK TABLES;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password_hash` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'pete','6cb912de5b6ef87cb3ecf92c13052d80d02ed91771e6e640625d759cd444e941');
+INSERT INTO `users` VALUES (4,'ppp','c4289629b08bc4d61411aaa6d6d4a0c3c5f8c1e848e282976e29b6bed5aeedc7');
+INSERT INTO `users` VALUES (5,'admin','08f2b4a83c6d31deef5357b7956fde1086c7fbbf5c8d11b879bbae880806d0fd');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
